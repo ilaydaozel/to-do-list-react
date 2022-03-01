e27d7ebf2e646319d70868773a95d073dd2cd0bd
ilaydaozel <ilaydaoozel@gmail.com>
2022-03-01 21:28:31 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function App() {
+  const [item, setItem] = useState("");
+  const [itemArray, setItemArray] = useState([]);
+
+  function handleChange(event) {
+    const newItem = event.target.value;
+    setItem(newItem);
+  }
+
+  function handleClick() {
+    setItemArray(function () {
+      return [...itemArray, item];
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+
+      <div className="form">
+        <input onChange={handleChange} type="text" value={item} />
+        <button onClick={handleClick}>
+          <span>Add</span>{" "}
+        </button>
+      </div>
+
+      <div>
+        <ul>
+          {console.log(itemArray)}
+          {itemArray.map((elm) => (
+            <li>{elm}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
